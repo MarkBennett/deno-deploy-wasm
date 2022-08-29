19661324c5204af2044d509e8f6b1941b1975004
Mark Bennett <mark@burmis.ca>
2022-08-29 16:05:32 -0600

    Parse the size from the request

--- main.ts
+++ main.ts
@@ -1,18 +1,35 @@
 import { serve } from "https://deno.land/std@0.145.0/http/server.ts";
 import { resize } from "https://deno.land/x/deno_image@0.0.4/mod.ts";
 
-const imgFile = await Deno.readFile("./deno.jpg");
-const img = await resize(imgFile, {
-  width: 100,
-  height: 100,
-});
+async function getImageData(): Promise<Uint8Array> {
+  const imgFile = await Deno.readFile("./deno.jpg");
+  return imgFile;
+}
 
-serve(
-  (_req: Request) =>
-    new Response(img.buffer, {
-      status: 200,
-      headers: {
-        "content-type": "image/png",
-      },
-    })
-);
+async function resizeImg(
+  imgFile: Uint8Array,
+  size: number
+): Promise<Uint8Array> {
+  const img = await resize(imgFile, {
+    width: size,
+    height: size,
+  });
+  return img;
+}
+
+serve(async (req: Request) => {
+  // Parse size from request
+  const params = new URL(req.url).searchParams;
+  const sizeStr = params.get("size") || "100";
+  const size = parseInt(sizeStr);
+
+  const img = await getImageData();
+  const resizedImg = await resizeImg(img, size);
+
+  return new Response(resizedImg.buffer, {
+    status: 200,
+    headers: {
+      "content-type": "image/jpg",
+    },
+  });
+});
